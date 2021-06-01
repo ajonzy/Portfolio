f15677354ba0e02c1e3a41f3f71f47323b207c9d
alexjones <ajonzy@gmail.com>
2021-06-01 10:04:32 -0600

    Initial navbar styles

--- src/components/components/navbar.js
+++ src/components/components/navbar.js
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom"
 export default function navbar() {
     return (
         <div className='navbar-wrapper'>
+            <span>Alex Jones</span>
             <NavLink exact to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/projects">Projects</NavLink>
