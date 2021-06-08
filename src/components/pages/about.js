dbd5a91db956e4b4a51354d428246fd936d6f0ae
alexjones <ajonzy@gmail.com>
2021-06-08 10:44:42 -0600

    Inital styles for the about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -9,9 +9,13 @@ export default function about() {
 
             <img src={Profile} alt=""/>
 
-            <p>For over two years I have been an instructor an Bottega University, having the amazing opportunity to teach students how to code. Every day I have have been immersed in the coding process, spanning all the way from simple variables, data types, and functions; to building out APIs, utilizing popular frameworks, and building fully responsive apps. It's a truly rewarding experience to watch a new student with no prior experience join my class, start with the basics and diligently work their way up, graduate, and find employment in their dream industry. It's now my turn.</p>
+            <p>For over two years I have been an instructor an Bottega University, having the amazing opportunity to teach students how to code. Every day I have have been immersed in the coding process, spanning all the way from simple variables, data types, and functions; to building out APIs, utilizing popular frameworks, and building fully responsive apps. It's a truly rewarding experience to watch a new student with no prior experience join my class, start with the basics and diligently work their way up, graduate, and find employment in their dream industry.</p>
 
-            <p>I am now seeking a new opportunity to advance my career. I bring with me a solid understanding of coding foundations, best practices, and industry standards using languages such as HTML, CSS, JavaScript, and Python. I have experience building full-stack applications with libraries such as React and Flask. I've utilized many different databases to handle information, including SQL, MongoDB, and Redis. And I am so excited about what new skills I'll learn in the future!</p>
+            <p>It's now my turn.</p>
+
+            <p>I am now seeking a new opportunity to advance my career. I bring with me a <span>solid understanding of coding foundations</span>, <span>best practices</span>, and <span>industry standards</span> using languages such as <span>HTML</span>, <span>CSS</span>, <span>JavaScript</span>, and <span>Python</span>. I have experience building <span>full-stack applications</span> with libraries such as <span>React</span> and <span>Flask</span>. I've utilized many different <span>databases</span> to handle information, including <span>SQL</span>, <span>MongoDB</span>, and <span>Redis</span>. And I've done all this while <span>communicating effictively</span> so that my students could follow along and in turn replicate these skills on their own.</p>
+
+            <p>And I am so excited about what new skills I'll learn in the future!</p>
         </div>
     )
 }
